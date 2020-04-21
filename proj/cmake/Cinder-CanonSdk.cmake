e3eb6e50c1b77e9a6fdcdb3e1774837272931e57
Maanav Patel <maanav.patel18@icloud.com>
2020-04-21 00:32:19 -0500

    initial cmake file

---
+++ proj/cmake/Cinder-CanonSdk.cmake
@@ -0,0 +1,39 @@
+if(NOT TARGET Cinder-CanonSdk)
+    # Define ${Cinder-CanonSdk_PROJECT_ROOT}. ${CMAKE_CURRENT_LIST_DIR} is just the current directory.
+    get_filename_component(Cinder-CanonSdk_PROJECT_ROOT "${CMAKE_CURRENT_LIST_DIR}/../.." ABSOLUTE)
+
+    # Define ${CINDER_PATH} as usual.
+    get_filename_component(CINDER_PATH "${Cinder-CanonSdk_PROJECT_ROOT}/../.." ABSOLUTE)
+
+    # Make a list of source files and define that to be ${SOURCE_LIST}.
+    file(GLOB SOURCE_LIST CONFIGURE_DEPENDS
+            "${Cinder-CanonSdk_PROJECT_ROOT}/src/CanonCamera.cpp"
+            "${Cinder-CanonSdk_PROJECT_ROOT}/src/CinderCanon.cpp"
+            )
+
+    # Create the library!
+    add_library(Cinder-CanonSdk ${SOURCE_LIST})
+
+    # Add include directories.
+    # Notice that `cinderblock.xml` has `<includePath>src</includePath>`.
+    # So you need to set `../../src/` to include.
+    target_include_directories(Cinder-CanonSdk PUBLIC "${Cinder-CanonSdk_PROJECT_ROOT}/src" )
+    target_include_directories(Cinder-CanonSdk SYSTEM BEFORE PUBLIC "${CINDER_PATH}/include" )
+    target_include_directories(Cinder-CanonSdk SYSTEM BEFORE PUBLIC "${Cinder-CanonSdk_PROJECT_ROOT}/lib/EDSDK/Header" )
+
+
+
+    # If your Cinder block has no source code but instead pre-build libraries,
+    # you can specify all of them here (uncomment the below line and adjust to your needs).
+    # Make sure to use the libraries for the right platform.
+#    target_link_libraries(Cinder-CanonSdk "${Cinder-OpenCV_PROJECT_ROOT}/lib/libopencv_core.a")
+
+    if(NOT TARGET cinder)
+        include("${CINDER_PATH}/proj/cmake/configure.cmake")
+        find_package(cinder REQUIRED PATHS
+                "${CINDER_PATH}/${CINDER_LIB_DIRECTORY}"
+                "$ENV{CINDER_PATH}/${CINDER_LIB_DIRECTORY}")
+    endif()
+    target_link_libraries(Cinder-CanonSdk PRIVATE cinder)
+
+endif()
